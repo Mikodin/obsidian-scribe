dd22f816d7061b45a7a534d693ffc18c496ba8a7
Mike A <5902658+Mikodin@users.noreply.github.com>
2025-01-20 15:38:42 -0500

    remove console log

--- src/util/filenameUtils.ts
+++ src/util/filenameUtils.ts
@@ -3,7 +3,6 @@ import { moment } from "obsidian";
 export function formatFilenamePrefix(prefix: string, format: string) {
 	if (prefix.includes("{{date}}")) {
 		const formatted = prefix.replace("{{date}}", moment().format(format));
-		console.log("includes date, formatting", prefix, format, formatted);
 		return formatted;
 	}
 	return prefix;
