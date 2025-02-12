83f32396e63568c5534543c66a20a267e8d3b549
Mike A <5902658+Mikodin@users.noreply.github.com>
2025-02-11 21:31:35 -0500

    Adds audioFileLanguage to scribeOptions for temp switching

--- src/modal/scribeControlsModal.tsx
+++ src/modal/scribeControlsModal.tsx
@@ -57,6 +57,7 @@ const ScribeModal: React.FC<{ plugin: ScribePlugin }> = ({ plugin }) => {
     isOnlyTranscribeActive: plugin.settings.isOnlyTranscribeActive,
     isSaveAudioFileActive: plugin.settings.isSaveAudioFileActive,
     isMultiSpeakerEnabled: plugin.settings.isMultiSpeakerEnabled,
+    audioFileLanguage: plugin.settings.audioFileLanguage,
   });
 
   const hasOpenAiApiKey = Boolean(plugin.settings.openAiApiKey);
