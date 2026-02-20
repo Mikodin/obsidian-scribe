6f5cb119d1a822cd4e801ce94e59ca96ee6d2175
Mike A <5902658+Mikodin@users.noreply.github.com>
2026-02-20 12:37:48 -0500

    Fixes assemblyAi speech_models sometimes being empty (#94)

--- src/util/assemblyAiUtil.ts
+++ src/util/assemblyAiUtil.ts
@@ -1,7 +1,7 @@
 import {
   AssemblyAI,
-  type TranscriptParagraph,
   type TranscribeParams,
+  type TranscriptParagraph,
 } from 'assemblyai';
 import type { ScribeOptions } from 'src';
 import { LanguageOptions } from './consts';
@@ -23,6 +23,8 @@ export async function transcribeAudioWithAssemblyAi(
     audio: audioBuffer,
     format_text: true,
     speaker_labels: isMultiSpeakerEnabled,
+    /** Choosing universal-3-pro as default, cost is similar.  Will fallback to Universal-2 if language is not supported */
+    speech_models: ['universal-3-pro', 'universal-2'],
   };
 
   const params = useAudioFileLanguageSetting
